29f47bbdc0d7017a533640d96bc37f1bb763bdf7
OnyxPerseus <nguyenminhquang2406@gmail.com>
2025-06-20 13:09:45 +0700

    format code for largest rectangle area problem

--- largest-rectangle-area.py
+++ largest-rectangle-area.py
@@ -4,9 +4,14 @@ def solve(heights):
     stack = []
 
     for i in range(n + 1):
-        while stack and (i == n  or heights[stack[-1]] >= heights[i]):
+        while stack and (i == n or heights[stack[-1]] >= heights[i]):
             height = heights[stack.pop()]
             width = i if not stack else i - stack[-1] - 1
             maxArea = max(maxArea, height * width)
         stack.append(i)
     return maxArea
+
+
+if __name__ == "__main__":
+    heights = [2, 1, 5, 6, 2, 3]
+    print(solve(heights))
